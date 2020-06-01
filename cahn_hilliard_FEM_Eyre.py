4a290a8d223ec70bbb7d5ff850f3d423479c68e5
Dani <dani9.acost@gmail.com>
2020-06-01 19:03:34 +0200

    Primeras pruebas

--- cahn_hilliard_FEM_Eyre.py
+++ cahn_hilliard_FEM_Eyre.py
@@ -9,60 +9,64 @@ We will comupute the energy functional E=\int_\omega u^2 in each time step
 """
 
 from __future__ import print_function
-from dolfin import *
+from fenics import *
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 
-T = 2.0            # final time
-num_steps = 10     # number of time steps
+T = 1.0            # final time
+num_steps = 50     # number of time steps
 dt = T / num_steps # time step size
 eps = 0.01
-gamma = 0.0001
+gamma = 0.00001
 
 # Create mesh and define function space
-nx = ny = 10 # Boundary points
+nx = ny = 100 # Boundary points
 mesh = UnitSquareMesh(nx,ny)
 
 plot(mesh)
 plt.show()
 
 deg = 1 # Degree of polynomials in discrete space
-V = FunctionSpace(mesh, "Lagrange", deg)
+P = FiniteElement("Lagrange", mesh.ufl_cell(), deg) # Space of polynomials
+W = FunctionSpace(mesh, MixedElement([P,P])) # Space of functions
 
-# Define boundary condition
-u_D = Constant(0)
+# Random initial data
+u_0 =  Expression(('0.02*(0.5- rand())','0.02*(0.5- rand())'), degree=deg) # Random values between -0.01 and 0.01
+u_n = interpolate(u_0,W)
 
-def boundary(x, on_boundary):
-    return on_boundary
-
-bc = DirichletBC(V, u_D, boundary)
-
-random.seed(867658767987)
-u_0 =  Expression('0.02*(0.5- rand())', degree=deg) # Random values between -0.01 and 0.01
-u_n = interpolate(u_0,V)
-print('max = %f' % (u_n.vector().get_local().max()))
-print('min = %f' % (u_n.vector().get_local().min()))
-c = plot(u_n)
+phi_n,w_n = u_n.split(True)
+print('max = %f' % (phi_n.vector().get_local().max()))
+print('min = %f' % (phi_n.vector().get_local().min()))
+c = plot(phi_n)
 plt.colorbar(c)
 plt.show()
 
-w_n = - eps**2 * div(grad(u_n)) + pow(u_0,3) - 3 * pow(u_0,2) + 2 * u_0
-plot(w_n)
+w_n = - eps**2 * div(grad(phi_n)) + pow(phi_n,3)  + 2 * phi_n
+c = plot(w_n)
+plt.colorbar(c)
 plt.show()
 
 # Define the energy vector
 E = []
 
 # Define variational problem
-u = TrialFunction(V) # Meaningless function used to define the variational formulation
-v = TestFunction(V) # Meaningless function used to define the variational formulation
+u = TrialFunction(W) # Meaningless function used to define the variational formulation
+v = TestFunction(W) # Meaningless function used to define the variational formulation
 
-a1 = u*v*dx + dt*dot(grad(u), grad(v))*dx
-L = u_n*v*dx
+phi, w = split(u)
+barphi, barw = split(v)
+
+a1 = phi * barw * dx + dt * gamma * dot(grad(w),grad(barw)) * dx
+L1 = phi_n * barw * dx
+
+a2 = w * barphi * dx - pow(eps,2) * dot(grad(phi),grad(barphi)) * dx - 2 * phi * barphi * dx
+L2 = pow(phi_n,3) * barphi * dx - 3 * phi_n * barphi * dx
+
+a = a1 + a2
+L = L1 + L2
 
 # Time-stepping
-u = Function(V)
+u = Function(W)
 t = 0
 for n in range(num_steps):
 
@@ -70,27 +74,35 @@ for n in range(num_steps):
     t += dt
 
     # Compute solution
-    solve(a == L, u, bc)
+    solve(a == L, u)
+
+    phi, w = u.split(True)
 
     # Plot solution
-    pic = plot(u)
+    #pic = plot(phi)
     # ,mode='color')
-    plt.title("Ecuación del calor en t = %.2f" %(t))
-    plt.colorbar(pic)
-    plt.show()
+    #plt.title("Ecuación del Cahn-Hilliard en t = %.2f" %(t))
+    #plt.colorbar(pic)
+    #plt.show()
 
 
     # Compute the energy
-    energy = assemble(0.5*u*u*dx)
-    E.append(energy)
-    print('E =',energy)
+    #energy = assemble(0.5*u*u*dx)
+    #E.append(energy)
+    #print('E =',energy)
 
 
     # Update previous solution
-    u_n.assign(u)
+    phi_n = phi
 
-plt.plot(np.linspace(0,T,num_steps),E, color='red')
-plt.title("Funcional de energía")
-plt.xlabel("Tiempo")
-plt.ylabel("Energía")
+#plt.plot(np.linspace(0,T,num_steps),E, color='red')
+#plt.title("Funcional de energía")
+#plt.xlabel("Tiempo")
+#plt.ylabel("Energía")
+#plt.show()
+print('max = %f' % (phi.vector().get_local().max()))
+pic = plot(phi)
+# ,mode='color')
+plt.title("Ecuación del Cahn-Hilliard en t = %.2f" %(t))
+plt.colorbar(pic)
 plt.show()
