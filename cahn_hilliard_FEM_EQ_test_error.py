8e642d9e931f496046798e844b24add485caf8c4
Dani <dani9.acost@gmail.com>
2020-07-02 20:41:10 +0200

    Guardo más imágenes

--- cahn_hilliard_FEM_EQ_test_error.py
+++ cahn_hilliard_FEM_EQ_test_error.py
@@ -22,7 +22,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 T = 0.1            # final time
-num_steps = 100     # number of time steps
+num_steps = 200     # number of time steps
 dt = T / num_steps # time step size
 eps = Constant(0.1)
 gamma = Constant(1.0)
@@ -31,7 +31,7 @@ B  = Constant(1.0)
 print("dt = %d" %(dt))
 
 # Create mesh and define function space
-nx = ny = 200 # Boundary points
+nx = ny = 155 # Boundary points
 print("nx = ny = %f" %(nx))
 
 mesh = RectangleMesh(Point(-pi,3*pi), Point(3 * pi, -pi), nx, ny, "right/left")
